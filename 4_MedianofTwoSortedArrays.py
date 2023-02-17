ecf77f7a881bb4706c692ec32993b6d1eddfc937
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-02-17 10:53:17 -0800

    Update 4_MedianofTwoSortedArrays.py

--- 4_MedianofTwoSortedArrays.py
+++ 4_MedianofTwoSortedArrays.py
@@ -1,43 +1,37 @@
-# Python program to merge two sorted arrays/
-def mergeArrays(arr1, arr2):
-    i = 0
-    j = 0
-    k = 0
-    arr3 = [0 for i in range(len(arr1)+len(arr2))]
-    #mergeArrays(arr1, arr2, arr3)
-  
-    # traverse the arr1 and insert its element in arr3
-    while(i < len(arr1)):
-        arr3[k] = arr1[i]
-        k += 1
-        i += 1
-  
-    # now traverse arr2 and insert in arr3
-    while(j < len(arr2)):
-        arr3[k] = arr2[j]
-        k += 1
-        j += 1
-  
-    # sort the whole array arr3
-    arr3.sort()
+class Solution(object):
+    def median(self,lst):
+        n = len(lst)
+        s = sorted(lst)
+        return (s[n//2-1]/2.0+s[n//2]/2.0, s[n//2])[n % 2] if n else None
     
-    return arr3
-  
-  
-# Driver code
-
-arr1 = [1, 3, 5, 7]
-arr2 = [2, 4, 6, 8]
-arr3=mergeArrays(arr1, arr2)
-print("Array after merging")
-for val in arr3:
-    print(val)
-    
-def median(lst):
-    n = len(lst)
-    s = sorted(lst)
-    return (s[n//2-1]/2.0+s[n//2]/2.0, s[n//2])[n % 2] if n else None
-    
-a=median(arr3)
-
-print(a)
+    def findMedianSortedArrays(self, nums1, nums2):
+        i = 0
+        j = 0
+        k = 0
+        arr3 = [0 for i in range(len(nums1)+len(nums2))]
+        #mergeArrays(arr1, arr2, arr3)
+      
+        # traverse the arr1 and insert its element in arr3
+        while(i < len(nums1)):
+            arr3[k] = nums1[i]
+            k += 1
+            i += 1
+      
+        # now traverse arr2 and insert in arr3
+        while(j < len(nums2)):
+            arr3[k] = nums2[j]
+            k += 1
+            j += 1
+      
+        # sort the whole array arr3
+        arr3.sort()
+        
+        a=self.median(arr3)
+        return a
+        
+        
+#nums1 = [1,2]
+#nums2 = [3,4]
+ans=Solution().findMedianSortedArrays(nums1 = [1,3], nums2 = [2])
+print(ans)
+            
