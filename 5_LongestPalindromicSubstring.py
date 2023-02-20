d0b8cab1690c330447a9f90ed4eb1ea996b2855a
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-02-19 19:03:01 -0800

    Update 5_LongestPalindromicSubstring.py

--- 5_LongestPalindromicSubstring.py
+++ 5_LongestPalindromicSubstring.py
@@ -1,15 +1,8 @@
 class Solution(object):
     def longestPalindrome(self,s) :
         res = [  s[i: j]  for i in range(len(s)) for j in range(i + 1, len(s) + 1) if  s[i: j] ==  s[i: j][::-1]]
-        pal = {}
-        
-        #print(a_dict)
-        for palabra in res:
-            pal[palabra] = len(palabra)
-        #print(pal)       
-        #print(max(pal.values()))
-        max_value = max(pal, key=pal.get)
-        return max_value
+
+        return max(res, key=len)
         
         
                 
