c0196d025435cb7041ebba5f9d86c70e350683d4
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-02-20 10:26:29 -0800

    Update 5_LongestPalindromicSubstring.py

--- 5_LongestPalindromicSubstring.py
+++ 5_LongestPalindromicSubstring.py
@@ -1,13 +1,69 @@
+# Python program to implement Manacher's Algorithm
 class Solution(object):
-    def longestPalindrome(self,s) :
-        res = [  s[i: j]  for i in range(len(s)) for j in range(i + 1, len(s) + 1) if  s[i: j] ==  s[i: j][::-1]]
+    def longestPalindrome(self,s):
+        N = len(s)
+        if N == 0:
+            return
+        N = 2*N+1    # Position count
+        L = [0] * N
+        L[0] = 0
+        L[1] = 1
+        C = 1     # centerPosition
+        R = 2     # centerRightPosition
+        i = 0    # currentRightPosition
+        iMirror = 0     # currentLeftPosition
+        maxLPSLength = 0
+        maxLPSCenterPosition = 0
+        start = -1
+        end = -1
+        diff = -1
+      
+        # Uncomment it to print LPS Length array
+        # printf("%d %d ", L[0], L[1]);
+        for i in range(2,N):
+          
+            # get currentLeftPosition iMirror for currentRightPosition i
+            iMirror = 2*C-i
+            L[i] = 0
+            diff = R - i
+            # If currentRightPosition i is within centerRightPosition R
+            if diff > 0:
+                L[i] = min(L[iMirror], diff)
+      
+            # Attempt to expand palindrome centered at currentRightPosition i
+            # Here for odd positions, we compare characters and
+            # if match then increment LPS Length by ONE
+            # If even position, we just increment LPS by ONE without
+            # any character comparison
+            try:
+                while ((i + L[i]) < N and (i - L[i]) > 0) and \
+                        (((i + L[i] + 1) % 2 == 0) or \
+                        (s[(i + L[i] + 1) // 2] == s[(i - L[i] - 1) // 2])):
+                    L[i]+=1
+            except Exception as e:
+                pass
+      
+            if L[i] > maxLPSLength:        # Track maxLPSLength
+                maxLPSLength = L[i]
+                maxLPSCenterPosition = i
+      
+            # If palindrome centered at currentRightPosition i
+            # expand beyond centerRightPosition R,
+            # adjust centerPosition C based on expanded palindrome.
+            if i + L[i] > R:
+                C = i
+                R = i + L[i]
+      
+        # Uncomment it to print LPS Length array
+        # printf("%d ", L[i]);
+        if(len(s)==1):
+            return s
+        start = (maxLPSCenterPosition - maxLPSLength) // 2
+        end = start + maxLPSLength - 1
+        return (s[start:end+1])
+  
+# Driver program
+text1 = "b"
+a=Solution().longestPalindrome(text1)
 
-        return max(res, key=len)
-        
-        
-                
-   
-   
-
-w=Solution().longestPalindrome("fkyidosnupvunmklebjiepwdmfhqjfjgtcdivzgibcewxviirtneumvhcwzvstvtnzrnzknehahdipswtvgmqhmexnjtqcpngvojdxmhwqhrdcgybehvrfsqkroaztrhyeuuzkthfhwtbfnyghlzjqsqjpqvsrkabcxylpgylzzgyzmhruqyezfcvzcmzzuvtxlbfyukhvnytetagrhsebodddqiowahvflakfkefzlwkdjyxtymypkqkeniriybvdcfnqogilpeiviatavcbtxogxenbfhpfqklrekqefzjunpzrenqhorpqnhxllceubkndibdypbmbjscnryafertbursmghissihgmsrubtrefayrncsjbmbpydbidnkbuecllxhnqprohqnerzpnujzfeqkerlkqfphfbnexgoxtbcvataiviepligoqnfcdvbyirinekqkpymytxyjdkwlzfekfkalfvhawoiqdddobeshrgatetynvhkuyfblxtvuzzmczvcfzeyqurhmzygzzlygplyxcbakrsvqpjqsqjzlhgynfbtwhfhtkzuueyhrtzaorkqsfrvhebygcdrhqwhmxdjovgnpcqtjnxemhqmgvtwspidhahenkznrzntvtsvzwchvmuentriivxwecbigzvidctgjfjqhfmdwpeijbelkmnuvpunsodiykf")
-print(w) 
+print(a)
