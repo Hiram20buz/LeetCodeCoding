ca44772434d3a2e573f2192324a87c267c5af35c
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-05 13:36:24 -0700

    Update 11_ContainerWithMostWater.py

--- 11_ContainerWithMostWater.py
+++ 11_ContainerWithMostWater.py
@@ -40,28 +40,45 @@ class Solution:
         
     def maxArea(self, height: list[int]) -> int:
         result=[]
-        if(self.print2largest(height, len(height))!=0):
-            dif=self.indices(height, max(height))
-            base=dif[len(dif)-1]-dif[0]
-            print(base)
-            result.append(base*max(height))
-            
-        print(self.print2largest(height, len(height))==0)
+        '''
         if(self.print2largest(height, len(height))==0):
-            n = len(height)
-            dif1=self.indices(height,self.print2largest(height, n) )
-            base2=dif1[len(dif1)-1]-dif1[0]
-            result.append(base2*self.print2largest(height, n))
+            dis=self.indices(height, max(height))
+            base=dis[len(dis)-1]-dis[0]
+            result.append(base*max(height))
+            return result[0]
+        '''  
+        if(len(height)==2):
+            dis=self.indices(height, max(height))
+            base=dis[len(dis)-1]-dis[0]
+            result.append(1*min(height))
+            result.append(base*max(height))
+            return max(result)
+            
+            
+        dif=self.indices(height, max(height))
+        base=dif[len(dif)-1]-dif[0]
+        #print(base)
+        result.append(base*max(height))
+        
         n = len(height)
+        dif1=self.indices(height,self.print2largest(height, n) )
+        base2=dif1[len(dif1)-1]-dif1[0]
+        result.append(base2*self.print2largest(height, n))
+            
+         
         if(min(self.indices(height, max(height))) < min(self.indices(height,self.print2largest(height, n) ))):
             base3=self.indices(height,self.print2largest(height, n) )[len(self.indices(height,self.print2largest(height, n) ))-1]-self.indices(height, max(height))[0]
             result.append(base3*self.print2largest(height, n))
+            
+        
+        #print(self.print2largest(height, len(height))==0)
+    
             
         
         return max(result)
         
       
   
-a=Solution().maxArea([4,3,2,1,4])
+a=Solution().maxArea([1,2,4,3])
 
 print(a)
