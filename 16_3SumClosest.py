273a269c55988f6001906ab153086a1fb94b3934
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-05 16:59:13 -0700

    Create 16_3SumClosest.py

---
+++ 16_3SumClosest.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def threeSumClosest(self,nums: list[int], target: int) -> int:
+        # Sort the given array
+        nums.sort()
+        # Length of the array
+        n = len(nums)
+        # Closest value
+        closest = nums[0] + nums[1] + nums[n - 1]
+        # Loop for each element of the array
+        for i in range(0, n - 2):
+            # Left and right pointers
+            j = i + 1
+            k = n - 1
+            # Loop for all other pairs
+            while j < k:
+                current_sum = nums[i] + nums[j] + nums[k]
+                if current_sum <= target:
+                    j += 1
+                else:
+                    k -= 1
+                if abs(closest - target) > abs(current_sum - target):
+                    closest = current_sum
+        return closest
+        
+a=Solution().threeSumClosest([-1,0,1,2,-1,-4],0)
+print(a)
