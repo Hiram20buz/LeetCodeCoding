2be6ca007a1c23263a90f9ec516c5a3d264b725f
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-05 11:55:33 -0700

    Update 10_RegularExpressionMatching.py

--- 10_RegularExpressionMatching.py
+++ 10_RegularExpressionMatching.py
@@ -1,1 +1,22 @@
 # Regular Expression Matching
+# Importing re module
+import re
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        #return re.fullmatch(p,s)
+        return True if re.fullmatch(p,s) else False
+        
+a=Solution().isMatch("aa","a*")
+print(a)
+
+'''
+pattern="a*"
+
+s= "aa"
+		
+if(re.fullmatch(pattern,s)):
+    print("**Match found** ")
+else:
+    print("Match not found ") 
+'''
