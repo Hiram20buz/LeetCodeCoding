5680de5c606ac5cd9cfe3704de2f7ff7549f138c
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-05 17:03:19 -0700

    Create 17_LetterCombinationsOfAPhoneNumber.py

---
+++ 17_LetterCombinationsOfAPhoneNumber.py
@@ -0,0 +1,7 @@
+#17. Letter Combinations of a Phone Number
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        
+        
+a=Solution().letterCombinations("")
+print(a)
