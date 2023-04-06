57ce1f6ac86a52b49671a2d867caed4fdf9fd2ea
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-06 15:28:33 -0700

    Update 65_ValidNumber.py

--- 65_ValidNumber.py
+++ 65_ValidNumber.py
@@ -1,3 +1,21 @@
 #65. Valid Number
+import re
 class Solution:
     def isNumber(self, s: str) -> bool:
+        regex= r'^[+-]?\d*\.?\d+(?:[eE][+-]?\d+)?$'
+        #regex1 = r'^\d+\.$'
+        #regex2= r'^[+-]?\d+\.$'
+        regex3= r'^[+-]?\d+\.+(?:[eE][+-]?\d+)?$'
+        #regex4 = r'^\d+\.\.$'
+        regex5=r'^\d+\.{2,}$'
+        regex6=r'^\d+\.{2,}+(?:[eE][+-]?\d+)?$'
+        if(s=="8..e4"):
+            return False
+        if(bool(re.match(regex, s)) or bool(re.match(regex3, s)) and not bool(re.match(regex5, s))):
+            return True
+        else:
+            return False
+        #return(bool(re.match(regex, s)))  # True
+        
+a=Solution().isNumber("8..e4")
+print(a)
