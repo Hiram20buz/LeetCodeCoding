18d3271f366a6c30a6e1e3edd8d169cbf7ebef40
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-05 19:40:25 -0700

    Update 17_LetterCombinationsOfAPhoneNumber.py

--- 17_LetterCombinationsOfAPhoneNumber.py
+++ 17_LetterCombinationsOfAPhoneNumber.py
@@ -1,43 +1,41 @@
 #17. Letter Combinations of a Phone Number
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
+    def letterCombinations(self, digits: str) -> list[str]:
+        thisdict = {
+          2: ["a", "b", "c"],
+          3: ["d", "e", "f"],
+          4: ["g", "h", "i"],
+          5: ["j", "k", "l"],
+          6: ["m", "n", "o"],
+          7: ["p", "q", "r","s"],
+          8: ["t","u", "v"],
+          9: ["w", "x", "y","z"],
+        }
+        num=list(digits)
+        #print(num)
+        if(digits==""):
+            return([])
+            
+        if(len(num)==1):
+            return(thisdict[int(num[0])])
+            
+        if(len(num)==2):
+            list2 = [f'{a}{b}' for a in thisdict[int(num[0])] for b in thisdict[int(num[1])]]
+            return(list2)
+            
+        if(len(num)==3):
+            list2 = [f'{a}{b}' for a in thisdict[int(num[0])] for b in thisdict[int(num[1])]]
+            list3 = [f'{a}{b}' for a in list2 for b in thisdict[int(num[2])]]
+            return(list3)
+            
+        if(len(num)==4):
+            list2 = [f'{a}{b}' for a in thisdict[int(num[0])] for b in thisdict[int(num[1])]]
+            list3 = [f'{a}{b}' for a in list2 for b in thisdict[int(num[2])]]
+            list4 = [f'{a}{b}' for a in list3 for b in thisdict[int(num[3])]]
+            return(list4)
         
         
 a=Solution().letterCombinations("")
+
 print(a)
 
-thisdict = {
-  2: ["a", "b", "c"],
-  3: ["d", "e", "f"],
-  4: ["g", "h", "i"],
-  5: ["j", "k", "l"],
-  6: ["m", "n", "o"],
-  7: ["p", "q", "r","s"],
-  8: ["t","u", "v"],
-  9: ["w", "x", "y","z"],
-}
-#print(thisdict)
-def a(s):
-    num=list(s)
-    #print(num)
-    if(s==""):
-        print("")
-    if(len(num)==1):
-        print(thisdict[int(num[0])])
-        
-    if(len(num)==2):
-        list2 = [f'{a}{b}' for a in thisdict[int(num[0])] for b in thisdict[int(num[1])]]
-        print(list2)
-        
-    if(len(num)==3):
-        list2 = [f'{a}{b}' for a in thisdict[int(num[0])] for b in thisdict[int(num[1])]]
-        list3 = [f'{a}{b}' for a in list2 for b in thisdict[int(num[2])]]
-        print(list3)
-        
-    if(len(num)==4):
-        list2 = [f'{a}{b}' for a in thisdict[int(num[0])] for b in thisdict[int(num[1])]]
-        list3 = [f'{a}{b}' for a in list2 for b in thisdict[int(num[2])]]
-        list4 = [f'{a}{b}' for a in list3 for b in thisdict[int(num[3])]]
-        print(list4)
-    
-a("")
