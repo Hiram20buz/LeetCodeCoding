e481822be700a49a2e7ca014546238e34972a16c
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-06 11:54:24 -0700

    Update 50_Pow(x, n).py

--- 50_Pow(x, n).py
+++ 50_Pow(x, n).py
@@ -1,34 +1,59 @@
 #50. Pow(x, n)
 from decimal import Decimal
-x =  Decimal('2.00000')
-n = -2
+'''
+num = 2.00000
+formatted_num = "{:.5f}".format(num)
+#print(formatted_num)
+'''
+
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        num = "{:.5f}".format(x)
+        
+            
+        
+            
         y =pow(x, n)
         
-        decix=len(str(x))-len(str(int(x)))-1
-        #print(decix)
+        print(y)
+        dis=len(str(num).split(".")[1])
+        print(dis)
         
-        deciy=len(str(y))-len(str(int(y)))-1
-        #print(deciy)
-        #print(y)
+        dis1=len(str(y).split(".")[1])
+        print(dis1)
         
         num=list(str(y))
-        #print(num)
-        if(deciy-decix > 0):
-            for val in range(deciy-decix):
-                num.pop()
-                
-        if(deciy-decix < 0):
-            #print(1)
-            for val in range(decix-deciy):
-                #print(deciy-decix)
+        
+        if(dis-dis1 > 0):
+            for val in range(dis-dis1):
                 num.append("0")
+                
+                
+        if(dis-dis1 < 0):
+            #print(1)
+            for val in range(dis1-dis):
+                #print(deciy-decix)
+                num.pop()
+        
               
         str1 = ""       
         for ele in num:
             str1 += ele
-        return(Decimal(str1))
+            
+        
+         
+        #return numx   
+        #return(Decimal(str1))
+        #return(float(str1))
+        #return round(Decimal(str1))
+        return y
+
              
-a=Solution().myPow(x,n)
+a=Solution().myPow(23.56502,-3)
 print(a)
+
+
+
+
+
