7a0a24b89e03573e3d91e6279f79d1d517dd3486
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-04-06 15:37:14 -0700

    Update 11_ContainerWithMostWater.py

--- 11_ContainerWithMostWater.py
+++ 11_ContainerWithMostWater.py
@@ -1,91 +1,25 @@
 # 11. Container With Most Water
-# la longoitud es la que importa no la altura
 class Solution:
-    def print2largest(self,arr, arr_size):
-        # There should be atleast
-        # two elements
-    	if (arr_size < 2):
-    	
-    		print(" Invalid Input ")
-    		return
-    	
-    
-    	first = second = -2147483648
-    	for i in range(arr_size):
-    	
-    		# If current element is
-    				# smaller than first
-    		# then update both
-    				# first and second
-    		if (arr[i] > first):
-    		
-    			second = first
-    			first = arr[i]
-    		
-    
-    		# If arr[i] is in
-    				# between first and
-    		# second then update second
-    		elif (arr[i] > second and arr[i] != first):
-    			second = arr[i]
-    	
-    	if (second == -2147483648):
-    		#print("There is no second largest element")
-    		return 0
-    	else:
-    		#print("The second largest element is", second)
-    		return second
-    		
-    def indices(self,lst, item):
-        return [i for i, x in enumerate(lst) if x == item]
-        
     def maxArea(self, height: list[int]) -> int:
-        result=[]
-        '''
-        if(self.print2largest(height, len(height))==0):
-            dis=self.indices(height, max(height))
-            base=dis[len(dis)-1]-dis[0]
-            result.append(base*max(height))
-            return result[0]
-        '''  
-        if(len(height)==2):
-            dis=self.indices(height, max(height))
-            base=dis[len(dis)-1]-dis[0]
-            result.append(1*min(height))
-            result.append(base*max(height))
-            return max(result)
-            
-            
-        dif=self.indices(height, max(height))
-        base=dif[len(dif)-1]-dif[0]
-        #print(base)
-        result.append(base*max(height))
-        
-        n = len(height)
-        dif1=self.indices(height,self.print2largest(height, n) )
-        base2=dif1[len(dif1)-1]-dif1[0]
-        result.append(base2*self.print2largest(height, n))
-            
+        A=height
+        l = 0
+        r = len(A) -1
+        area = 0
          
-        if(min(self.indices(height, max(height))) < min(self.indices(height,self.print2largest(height, n) ))):
-            base3=self.indices(height,self.print2largest(height, n) )[len(self.indices(height,self.print2largest(height, n) ))-1]-self.indices(height, max(height))[0]
-            result.append(base3*self.print2largest(height, n))
-        #print(self.print2largest(height, len(height))==0)
-        if(min(self.indices(height, max(height))) == len(height)-2):
-            base3=self.indices(height,self.print2largest(height, n) )[0]-self.indices(height, max(height))[0]
-            result.append(base3*max(height))
-    
-            
-        #print(result)
-        if(height[len(height)-1] > height[0] and max(height)==height[len(height)-1]):
-            base3=len(height)-1
-            #print(base3)
-            result.append(base3*height[0])
-            
-        return max(result)
-        
-      
-  
-a=Solution().maxArea([1,8,6,2,5,4,8,25,7])
-
-print(a)
+        while l < r:
+            # Calculating the max area
+            area = max(area, min(A[l],
+                            A[r]) * (r - l))
+         
+            if A[l] < A[r]:
+                l += 1
+            else:
+                r -= 1
+        return area
+ 
+# Driver code
+a = [1, 5, 4, 3]
+b = [3, 1, 2, 4, 5]
+ 
+c=Solution().maxArea(a)
+print(c)
