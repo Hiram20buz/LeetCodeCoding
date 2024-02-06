7b611667015179f2d270fa6a01b947b5a90d66cd
Hiram20buz <l20211847@tectijuana.edu.mx>
2024-02-05 19:31:06 -0800

    back

--- 77.1_Combinations.py
+++ 77.1_Combinations.py
@@ -1,8 +1,8 @@
 from itertools import combinations
 
-
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        return combinations(range(1, n+1), k)
+    def combine(self, n: int, k: int) -> list[list[int]]:
+        return list(combinations(range(1, n + 1), k))
+
 
 print(Solution().combine(4, 2))
